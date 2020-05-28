ef2a7c585e722c065c7d87d1ef2feea60f934928
白帝 <15609254040@163.com>
2020-05-28 18:05:17 +0800

    Squashed commit of the following:
    commit b6ec5007b0f82461b25e9dc45395442b9bc5fb93
    Author: 白帝 <15609254040@163.com>
    Date:   Thu May 28 17:57:30 2020 +0800
    
        增加了注册页面和登录后台未完全写完
    
    commit c7f5a8592297655fa99c7ab671f8b647428f231f
    Author: nilu321 <1305724862@qq.com>
    Date:   Wed May 27 18:17:36 2020 +0800
    
        新增userdao
    
    commit e20b6da6a718d494d12e1b64660cb761ad0eaa4b
    Author: nilu321 <1305724862@qq.com>
    Date:   Wed May 27 18:15:27 2020 +0800
    
        1
    
    # Conflicts:
    #	guidecar/src/main/java/com/xzy/controller/UserController.java
    #	guidecar/src/main/java/com/xzy/dao/UserDao.java
    #	guidecar/src/main/java/com/xzy/service/UserService.java
    #	guidecar/target/classes/.gitignore
    #	guidecar/target/m2e-wtp/web-resources/META-INF/maven/com.xinzhiyun/guidecar/pom.properties

--- guidecar/src/main/java/com/xzy/dao/UserDao.java
+++ guidecar/src/main/java/com/xzy/dao/UserDao.java
@@ -1,11 +1,39 @@
 package com.xzy.dao;
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 import com.xzy.entity.User;
 
+=======
+import org.apache.ibatis.annotations.Param;
+
+import com.xzy.entity.User;
+
+
+>>>>>>> b6ec5007b0f82461b25e9dc45395442b9bc5fb93
+=======
+import com.xzy.entity.User;
+
+>>>>>>> master
 public interface UserDao {
 	
 	User findById(int id);
 	
 	User findByUsername(String username);
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+    	
+	//注册
+	void update(User user);
+	//登录
+	//根据用户名和密码查询用户
+	void findByUsernameAndPwd(@Param("name") String name,@Param("password") String passwordd);
+	
+	
+	
+>>>>>>> b6ec5007b0f82461b25e9dc45395442b9bc5fb93
+=======
+>>>>>>> master
 	
 }
--- guidecar/src/main/java/com/xzy/service/UserService.java
+++ guidecar/src/main/java/com/xzy/service/UserService.java
@@ -12,14 +12,48 @@ public class UserService {
 	@Autowired
 	private UserDao userDao;
 	
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> master
 	
 	public User findById(int id) {
 		return userDao.findById(id);
 	}
 	
+<<<<<<< HEAD
+=======
+	//根据用户id查询用户
+	public User findById(int id) {
+		return userDao.findById(id);
+	}
+	//根据用户名查询用户
+>>>>>>> b6ec5007b0f82461b25e9dc45395442b9bc5fb93
+=======
+>>>>>>> master
 	public User findByUsername(String username) {
 		return userDao.findByUsername(username);
 	}
 	
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+	//新增用户
+	
+	public void update(User user) {
+		userDao.update(user);
+	}
+	//登录
+	//根据用户id和密码查询
+	public void findByUsernameAndPwd(String name,String password) {
+		userDao.findByUsernameAndPwd(name, password);
+	}
+
+	
+	
+	
+>>>>>>> b6ec5007b0f82461b25e9dc45395442b9bc5fb93
+=======
+>>>>>>> master
 
 }
