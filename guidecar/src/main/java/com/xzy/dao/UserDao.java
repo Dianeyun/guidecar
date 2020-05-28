b6ec5007b0f82461b25e9dc45395442b9bc5fb93
白帝 <15609254040@163.com>
2020-05-28 17:57:30 +0800

    增加了注册页面和登录后台未完全写完

--- guidecar/src/main/java/com/xzy/dao/UserDao.java
+++ guidecar/src/main/java/com/xzy/dao/UserDao.java
@@ -1,11 +1,23 @@
 package com.xzy.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.xzy.entity.User;
+
 
 public interface UserDao {
 	
 	User findById(int id);
 	
 	User findByUsername(String username);
+    	
+	//注册
+	void update(User user);
+	//登录
+	//根据用户名和密码查询用户
+	void findByUsernameAndPwd(@Param("name") String name,@Param("password") String passwordd);
+	
+	
+	
 	
 }
--- guidecar/src/main/java/com/xzy/service/UserService.java
+++ guidecar/src/main/java/com/xzy/service/UserService.java
@@ -12,14 +12,28 @@ public class UserService {
 	@Autowired
 	private UserDao userDao;
 	
-	
+	//根据用户id查询用户
 	public User findById(int id) {
 		return userDao.findById(id);
 	}
-	
+	//根据用户名查询用户
 	public User findByUsername(String username) {
 		return userDao.findByUsername(username);
 	}
+	
+	//新增用户
+	
+	public void update(User user) {
+		userDao.update(user);
+	}
+	//登录
+	//根据用户id和密码查询
+	public void findByUsernameAndPwd(String name,String password) {
+		userDao.findByUsernameAndPwd(name, password);
+	}
+
+	
+	
 	
 
 }
