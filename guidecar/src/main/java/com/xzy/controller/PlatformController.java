ca15276defb56faaa3f28a88113592bf2377d861
白帝 <15609254040@163.com>
2020-06-02 18:46:50 +0800

    Squashed commit of the following:
    commit 4684de07272cd6213b995158a7b4e0cd717a6882
    Author: 白帝 <15609254040@163.com>
    Date:   Tue Jun 2 17:59:46 2020 +0800
    
        修改文件名字，创建公司控制类
    
    commit a57c443b895c872f0d2a627c8ced9a779e65f07b
    Author: 白帝 <15609254040@163.com>
    Date:   Tue Jun 2 10:07:27 2020 +0800
    
        重构文件名字
    
    # Conflicts:
    #	guidecar/src/main/java/com/xzy/controller/CompanyController.java
    #	guidecar/target/m2e-wtp/web-resources/META-INF/maven/com.xinzhiyun/guidecar/pom.properties

--- guidecar/src/main/java/com/xzy/controller/PlatformController.java
+++ guidecar/src/main/java/com/xzy/controller/PlatformController.java
@@ -1,26 +1,29 @@
 package com.xzy.controller;
 
-<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import com.google.gson.Gson;
+import com.xzy.entity.Company;
+import com.xzy.entity.DataStatus;
+import com.xzy.service.CompanyService;
 /**
- * 公司控制器
+ * 平台控器器
  * @author J·Y
  *
  */
-@Controller
-@RequestMapping("company")
-public class CompanyController {
 
+@Controller
+@RequestMapping("/platform")
+public class PlatformController {
 	@Autowired
 	private CompanyService companyService;
 	/**
@@ -111,7 +114,7 @@ public class CompanyController {
 		return ds.toGson(ds);
 	}
 	/**
-	 * 打开修改页面
+	 * 打开修改公司页面
 	 * @param id 公司id值
 	 * @return 需要修改的公司的信息及修改页面
 	 */
@@ -123,6 +126,11 @@ public class CompanyController {
 		mv.setViewName("Company/companyModify");
 		return mv;
 	}
+	/**
+	 * 修改公司
+	 * @param company 需要修改公司的信息
+	 * @return  1-成功  0-失败
+	 */
 	@RequestMapping(value="/companyModify", produces = "application/json;charset=utf-8")
 	@ResponseBody
 	public String CompanyModify(Company company) {
